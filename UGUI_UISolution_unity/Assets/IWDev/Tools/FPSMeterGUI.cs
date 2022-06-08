51d5e8ea37c9f63b7c50c6224b185102ea957af5
IvanKurdakov <kis.flack.more.more@gmail.com>
2022-06-08 20:35:08 +0300

    - fixed FPSMeterGUI.cs to properly work in landscape rotations

--- UGUI_UISolution_unity/Assets/IWDev/Tools/FPSMeterGUI.cs
+++ UGUI_UISolution_unity/Assets/IWDev/Tools/FPSMeterGUI.cs
@@ -13,18 +13,41 @@ namespace IWDev.Tools
 		public string Prefix = "FPS: ";
 
 
-		private GUIStyle _FPSLabelStyle;
-		private Rect _FPSRect = new Rect();
+		private GUIStyle _fPSLabelStyle;
+		private Rect _fPSRect = new Rect();
+
+		private bool _isPortrait = true;
 
 		private void Awake()
 		{
 			Instance = this;
 
-			_FPSRect = new Rect(5, 
-				Screen.height - Screen.height * 0.025f-20, 
-				(int)Screen.width*0.25f, 
+			_isPortrait = true;
+
+			if ((float)Screen.height / (float)Screen.width < 1f)
+			{
+				_isPortrait = false;
+			}
+
+			if (_isPortrait)
+			{
+				_fPSRect = new Rect(5,
+				Screen.height - Screen.height * 0.025f - 20,
+				(int)Screen.width * 0.25f,
 				(int)Screen.height * 0.025f + 20);
+			}
+			else
+			{
+				_fPSRect = new Rect(5,
+					Screen.height - Screen.height * 0.025f - 50,
+					(int)Screen.width * 0.25f,
+					(int)Screen.height * 0.025f + 50);
+			}
+
+
 		}
+
+
 
 		private IEnumerator Start()
 		{
@@ -48,11 +71,13 @@ namespace IWDev.Tools
 			if (!CalculateFPS) return;
 
 
-			_FPSLabelStyle = new GUIStyle(GUI.skin.label);
-			_FPSLabelStyle.fontSize = (int)(Screen.width * 0.05f);
+			_fPSLabelStyle = new GUIStyle(GUI.skin.label);
+
+			if (_isPortrait) _fPSLabelStyle.fontSize = (int)(Screen.width * 0.05f);
+			else  _fPSLabelStyle.fontSize = (int)(Screen.height * 0.05f);
 
 			GUI.contentColor = FPSColor;
-			GUI.Label(_FPSRect, Prefix + CurrentFps.ToString(), _FPSLabelStyle);
+			GUI.Label(_fPSRect, Prefix + CurrentFps.ToString(), _fPSLabelStyle);
 		}
 		
 	
